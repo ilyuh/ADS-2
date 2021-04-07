b306d8ba6a6c7716f1379383cc13359a9eec644b
Ilya Fedotov <iluyha.uho@gmail.com>
2021-04-08 00:27:40 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,34 +1,5 @@
 // Copyright 2021 NNTU-CS
 
-// функция бинарного поиска
-int cbinsearch(int *arr, int size, int value) {
-  int left = 0;
-  int right = size - 1;
-  int counter = 0;
-
-  while (left < right) {
-    int mid = (left + right) / 2;
-    if (arr[mid] < value) {
-      left = mid + 1;
-    } else {
-      right = mid;
-    }
-  }
-
-  if (arr[left] == value) {
-    while (arr[left] == value) {
-      counter++;
-      left++;
-    }
-  }
-
-  if (counter != 0) {
-    return counter;
-  } else {
-    return 0;
-  }
-}
-
 int countPairs1(int *arr, int len, int value) {
   int counter = 0;
   for (int i = 0; i < len; i++) {
@@ -64,15 +35,39 @@ int countPairs2(int *arr, int len, int value) {
 }
 
 int countPairs3(int *arr, int len, int value) {
-  int counter = 0;
-  for (int i = 0; i < len; i++) {
-    int checker = value - arr[i];
-    counter = counter + cbinsearch(arr, len, checker);
-    if (arr[i] == checker) {
-      counter--;
-    }
-    arr[i] = 2147483647;
+  int left = 0, right = len, middle;
+  while (left < right - 1) {
+    middle = (left + right) / 2;
+    if (arr[middle] <= value)
+      left = middle + 1;
+    else
+      right = middle;
   }
-
+  len = right;
+  int counter = 0;
+  for (int i = 0; i < len - 1; i++) {
+    int left = i, right = len;
+    while (left < right - 1) {
+      middle = (left + right) / 2;
+      if (arr[i] + arr[middle] == value) {
+        counter++;
+        int j = middle + 1;
+        while (arr[i] + arr[j] == value && j < right) {
+          counter++;
+          j++;
+        }
+        j = middle - 1;
+        while (arr[i] + arr[j] == value && j > left) {
+          counter++;
+          j--;
+        }
+        break;
+      }
+      if (arr[i] + arr[middle] > value)
+        right = middle;
+      else
+        left = middle;
+    }
+  }
   return counter;
 }
